b46b36f7914530131d9d2beca3d0d0ad877684d7
Gilbert <gilbertduenas@users.noreply.github.com>
2020-07-20 08:23:46 +0900

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,22 @@
 if __name__ == '__main__':
+    answer = []
     N = int(input())
-
+    
     for i in range(N):
-        task = i.split()
+        task = input().split()
 
-        print(task)
+        if task[0] == 'append':
+            answer.append(int(task[1]))
+        elif task[0] == 'insert':
+            answer.insert(int(task[1]), int(task[2]))
+        elif task[0] == 'pop':
+            answer.pop()
+        elif task[0] == 'print':
+            print(answer)
+        elif task[0] == 'remove':
+            answer.remove(int(task[1]))
+        elif task[0] == 'reverse':
+            answer.reverse()
+        elif task[0] == 'sort':
+            answer.sort()
+            
